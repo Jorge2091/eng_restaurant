41930388139a53c4015197538806102bbfaeb54c
Jorge2091 <93310766+Jorge2091@users.noreply.github.com>
2022-10-13 09:28:06 +0100

    add while loop to python

--- restaurant.py
+++ restaurant.py
@@ -2,10 +2,23 @@
 menu = ["chicken soup", "roast duck with rice", "special fried rice", "pizza", "chips", "burger"]
 print("In our menu, we have {}, {}, {}, {}, {} and {}".format(menu[0], menu[1], menu[2], menu[3], menu[4], menu[5]))
 order_list = []
-order = input("What would you like to order? ")
-order_list.append(order)
-order = input("And? ")
-order_list.append(order)
-order = input("And? ")
-order_list.append(order)
-print(f"You have chosen {order_list[0]}, {order_list[1]} and {order_list[2]}")
+number = 1
+promt = True
+while promt:
+    order = input("what would you like to order? ")
+    order = order.lower()
+    if order in menu:
+        order_list.append(order)
+        while number < 3:
+            order = input("what else? ")
+            if order in menu:
+                order_list.append(order)
+                number += 1
+                if number == 2:
+                    promt = False
+            else:
+                print("That is not in the menu, I'm sorry. Please try again. ")
+    else:
+        print("That is not in a menu, I'm sorry. Please try again. ")
+
+
